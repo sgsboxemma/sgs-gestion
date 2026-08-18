a07e891231aa36c668b828d1ebdc28dc021b8586
sgsboxemma <sgsboxemma@gmail.com>
2026-08-19 00:42:00 +0200

    Utilisation des alias

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sgs-gestion-v9-4";
+const CACHE = "sgs-gestion-v9-5";
 const ASSETS = ["./", "./index.html", "./cloud.js", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
