57bdb760f293f57ecf9378e4df989e943f9b9a97
sgsboxemma <sgsboxemma@gmail.com>
2026-08-24 22:24:03 +0200

    V12.1 - ajout colonne Section exports

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sgs-gestion-v12";
+const CACHE = "sgs-gestion-v12-1";
 const ASSETS = ["./", "./index.html", "./cloud.js?v=10-2", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
