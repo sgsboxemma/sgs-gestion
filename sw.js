3bb1d0732c11cfae6c968c84e1064a3c61a3d564
sgsboxemma <sgsboxemma@gmail.com>
2026-09-12 08:26:13 +0200

    V15.4.5 - optimisation storage et egress renforcée

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE = "sgs-gestion-v15.4.1";
-const ASSETS = ["./", "./index.html", "./cloud.js?v=15.4.1", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE = "sgs-gestion-v15.4.5";
+const ASSETS = ["./", "./index.html", "./cloud.js?v=15.4.5", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => {
+  const keep = new Set([CACHE, "sgs-member-photo-blobs-v1"]);
+  return Promise.all(keys.filter(k => !keep.has(k)).map(k => caches.delete(k)));
+})));
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET" || new URL(event.request.url).origin !== location.origin) return;
   event.respondWith(fetch(event.request).then(response => {
